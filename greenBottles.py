b0c76d9677a463e804b6121360b8410a6ac58d8f
George Robson <grobson7094@sohamcollege.org>
2014-05-07 09:48:38 +0100

    Changed Green bottles
    does not work

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,8 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+bottles = int(input("How many green bottles?"))
+
+for x in range(bottles,-1,-1):
+    print("{0} green bottle hanging on the wall.\n{0} green bottles hanging on the wall.".format(x))
+    print("And if 1 green bottle should acidentally fall, \nThere'll be {0} green bottles hanging on the wall.\n".format(bottles-1))
+    
+    
+
